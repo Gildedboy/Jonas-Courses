65c35c5ce5fb321d2e2767bb6061bec3cb1b0149
Gildedboy <knukles30@gmail.com>
2023-03-08 22:26:40 -0700

    Starting of the Javascript file

---
+++ crash-course/today-i-learned/script.js
@@ -0,0 +1,17 @@
+console.log("Hello World!");
+
+const btn = document.querySelector(".btn-open");
+const form = document.querySelector(".fact-form");
+
+btn.addEventListener("click", function () {
+  if (form.classList.contains("hidden")) {
+    form.classList.remove("hidden");
+    btn.textContent = "Close";
+  } else {
+    form.classList.add("hidden");
+    btn.textContent = "Share a fact";
+  }
+});
+
+let votesInteresting = 23;
+const text = "Lisbon is the capital of Portugal";
